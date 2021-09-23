67a5afcd73742ed0a56ba3b95f83da85ae803f5e
B-P-THOMAS <65853504+B-P-THOMAS@users.noreply.github.com>
2021-09-23 18:24:36 +0100

    Update CShapeCollection.js
    I want to maintain an array of angles and then draw the SpokeAndBlob shape using the contents of that angles array.
    Using the angles array I can also draw the pie segments in different colours.

--- CShapeCollection.js
+++ CShapeCollection.js
@@ -1,34 +1,69 @@
 class CShapeCollection {
 
-    // In total there are 12 steps, one per semitone.
-    //
-    steps = [2, 2, 1, 2, 2, 2, 1];
-
     constructor(ctx, radius) {
+
+        // The numbers in the steps array represent the intervals in a 
+        // one-octave major scale:        
+        //        *  T  T  S  T  T  T  S
+        let steps = [2, 2, 1, 2, 2, 2, 1];
+
         this.selector = 6;
-        this.shapes = [];
+        this.angles = [];
+        this.lasttime = 0;
+        this.radius = radius;
 
         let idx = 0;
         let angle = -Math.PI / 2;
         do {
-            console.log(angle);
-            this.shapes.push(new CShape(radius, angle));
-            angle += this.steps[idx] * (Math.PI / 6);
-        } while (++idx < this.steps.length)
+            this.angles.push(angle);
+            angle += steps[idx] * (Math.PI / 6);
+        } while (++idx < steps.length)
     }
 
     render(ctx, time) {
-        for (let idx = 0; idx < this.shapes.length; ++idx) {
-            let t = (idx == this.selector) ? -time : 0;
-            this.shapes[idx].render(ctx, t);
+        let deltatime = time - this.lasttime;
+        this.lasttime = time;
+
+        this.angles[this.selector] -= (deltatime * Math.PI / 6);
+
+        for (let idx = 0; idx < this.angles.length; ++idx) {
+            this.renderSpokeAndBlob(ctx, this.angles[idx]);
         }
     }
 
+    renderSpokeAndBlob(ctx, angle) {
+
+        let width = ctx.canvas.width;
+        let height = ctx.canvas.height;
+
+        ctx.save();
+
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#325FA2';
+        ctx.fillStyle = '#ffffff';
+
+        ctx.translate(width / 2, height / 2);
+        ctx.rotate(angle);
+        ctx.translate(this.radius, 0);
+
+        ctx.beginPath();
+        ctx.moveTo(10 - this.radius, 0);
+        ctx.lineTo(0, 0);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.restore();
+    }
+
     advanceScene() {
-        this.shapes[this.selector].addAngle(Math.PI / -6);
         this.selector += 3;
-        this.selector = this.selector % this.shapes.length;
-        if (this.selector >= this.shapes.length) {
+        this.lasttime = 0;
+        this.selector = this.selector % this.angles.length;
+        if (this.selector >= this.angles.length) {
             this.selector = 0;
         }
     }
