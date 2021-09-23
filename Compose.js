3e783722d330a99cc010c298d8e3b3d75039e45d
B-P-THOMAS <65853504+B-P-THOMAS@users.noreply.github.com>
2021-09-23 10:59:38 +0100

    Update Compose.js
    Use an easing function to make the animation more "physical"

--- Compose.js
+++ Compose.js
@@ -5,6 +5,16 @@ let ctx = document.getElementById('canvas').getContext('2d');
 let bezel = new Bezel(ctx);
 let shapes = new CShapeCollection(ctx, bezel.radius);
 
+// Thanks to https://easings.net/
+//
+function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+}
+
+function easeOutExpo(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+}
+
 function doAnimation(timestamp) {
     // Calculate animation progress
     var progress = 0;
@@ -17,7 +27,7 @@ function doAnimation(timestamp) {
     // time is in the range [0.0, 1.0]
     //
     let time = progress / animationLength;
-    // console.log(time);
+    time = easeOutExpo(time);
 
     // Do animation ...
     let w = ctx.canvas.width;
@@ -29,9 +39,8 @@ function doAnimation(timestamp) {
     shapes.render(ctx, time);
     ctx.restore();
 
-//    if (progress < animationLength) {
-        requestAnimationFrame(doAnimation);
-//    }
+    requestAnimationFrame(doAnimation);
+
     if (progress > animationLength) {
         startTime = timestamp;
         shapes.advanceScene();
